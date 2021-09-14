ca29212161915a0aecbc5c2fe93c13fb3aa5f7bf
Brigham Andersen <brighamband@gmail.com>
2021-09-14 11:37:25 -0600

    cleaned up

--- proj1/fermat.py
+++ proj1/fermat.py
@@ -16,36 +16,32 @@ def mod_exp(x, y, N):                                               # O(n^3) for
     
 
 def fprobability(k):
-    return 1/(2**k)     # FIXME - Explain                           # O(n^2) ?? FIXME
+    return 1 - 1/(2**k)     # FIXME - Explain                           # O(n^2) ?? FIXME
 
 
 def mprobability(k):
-    return 1/(4**k)     # FIXME - Explain                           # O(n^2) ?? FIXME
+    return 1 - 1/(4**k)     # FIXME - Explain                           # O(n^2) ?? FIXME
+
 
 def prime_test1(N):
-    # pick a random number a that is between 0 and N (inclusive)
-    a = random.randint(0, N + 1)                                    # O(c)
-    if (a^(N-1)) % N == 1:                                          # FIXME
-        return "yes"                                                # O(c)
-    return "no"                                                     # O(c)
+    # pick a random positive number a that is between 1 and N (inclusive)
+    a = random.randint(1, N)                                      # O(c)
+    if a^(N-1) % N == 1:                                          # FIXME
+        print('ran here => a', a)
+        return True                                                # O(c)
+    print('reached end of pt1 -> a', a)
+    return False                                                     # O(c)
 
-def prime_test2(N, k):     # if not used directly in fermat
-    for i in range(k):
-        if prime_test1(N) == "no":
-            return "no"
-    return "yes"
 
-def fermat(N,k):
-    if prime_test2(N, k) == "yes":
-        return "prime"
-    # You will need to implement this function and change the return value, which should be
-    # either 'prime' or 'composite'.
-	#
-    # To generate random values for a, you will most likely want to use
-    # random.randint(low,hi) which gives a random integer between low and
-    #  hi, inclusive.
-    return "composite"
+def fermat(N,k):    # Essentially prime_test2
+    if prime_test1(N) == False:
+        return 'prime'
+    return 'composite'
 
+    # for i in range(k):
+    #     if prime_test1(N) == False:
+    #         return 'prime'
+    # return 'composite'
 
 def miller_rabin(N,k):
     # You will need to implement this function and change the return value, which should be
