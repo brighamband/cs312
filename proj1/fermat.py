62f0b7e847e02d53a294de8740521922a1d5aaaa
Brigham Andersen <brighamband@gmail.com>
2021-09-14 15:54:49 -0600

    extra code

--- proj1/fermat.py
+++ proj1/fermat.py
@@ -7,52 +7,55 @@ def prime_test(N, k):
 	return fermat(N,k), miller_rabin(N,k)
 
 
-def mod_exp(x, y, N):                                               # O(n^3) for function (O(n) for run through + O(n^2))
-    if y == 0:                                                      # O(c)
-        return 1                                                    # O(c)
-    z = mod_exp(x, math.trunc(y/2), N)   # truncate rounds down to nearest whole number                                       # O(n^2)
-    if y % 2 == 0:                                                  # O(c)
-        return (z**2) % N                                           # O(n^2)
-    return x*(z**2) % N                                            # O(n^2)
+def mod_exp(x, y, N):                                                   # O(n^3) for function (O(n) for run through + O(n^2))
+    if y == 0:      # Base case                                         # O(c)
+        return 1                                                        # O(c)
+    # Recursive call (trunc rounds down to nearest whole number)
+    z = mod_exp(x, math.trunc(y/2), N)                                  # O(n^2)
+    if y % 2 == 0:                                                      # O(c)
+        return (z**2) % N                                               # O(n^2)
+    return x*(z**2) % N                                                 # O(n^2)
     
 
 def fprobability(k):
-    return 1 - 1/(2**k)     # FIXME - Explain                           # O(n^2) ?? FIXME
+    return 1 - 1/(2**k)                                                 # O(n^2)
 
 
 def mprobability(k):
-    return 1 - 1/(4**k)     # FIXME - Explain                           # O(n^2) ?? FIXME
+    return 1 - 1/(4**k)                                                 # O(n^2)
 
 
 def f_prime_test(N):    # Essentially prime_test_1
-    a = 1 if N == 1 else random.randint(1, N - 1)             # 1 < a <= n - 1   
-    if mod_exp(a,N-1,N) == 1:   # Fermat's little theorem             # FIXME
-        return True                                                # O(c)
-    return False                                                     # O(c)
+    # a is random number a where 1 <= a < n 
+    a = 1 if N == 1 else random.randint(1, N - 1)  
+    if mod_exp(a,N-1,N) == 1:   # Fermat's little theorem               # O(n^3)
+        return True     # N is prime (fprobability likelihood)          # O(c)
+    return False        # N is composite (100%)                         # O(c)
 
 
 def fermat(N,k):    # Essentially prime_test2
-    for i in range(k):
-        if f_prime_test(N) == False:
-            return 'composite'
-    return 'prime'
+    for i in range(k):                                                  # O(n)
+        if f_prime_test(N) == False:                                    # O(c)
+            return 'composite'                                          # O(c)
+    return 'prime'                                                      # O(c)
 
 def m_prime_test(N): # Tests one number at a time
-    a = 1 if N == 1 else random.randint(1, N - 1)   # 1 < a <= n - 1
+    # a is random number a where 1 <= a < n 
+    a = 1 if N == 1 else random.randint(1, N - 1)
     exponent = N - 1
     while not exponent & 1:
         exponent >>= 1
     if mod_exp(a,exponent,N) == 1:
-        return True
+        return True     # N is prime (mprobability likelihood)
     while exponent < N - 1:
         if mod_exp(a,exponent,N) == N - 1:
-            return True
+            return True     # N is prime (mprobability likelihood)
         exponent <<= 1
-    return False
+    return False    # N is composite (100%)
 
 
 def miller_rabin(N,k):
-    for i in range(k):
-        if m_prime_test(N) == False:
-            return 'composite'
-    return 'prime'
+    for i in range(k):                                                  # O(n)
+        if m_prime_test(N) == False:                                    # O(c)
+            return 'composite'                                          # O(c)
+    return 'prime'                                                      # O(c)
