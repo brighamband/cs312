61536f6c0e2e148f3031fb6cde45b22e474899be
Brigham Andersen <brighamband@gmail.com>
2021-09-28 18:14:13 -0600

    more work, added hull class

--- proj2/convex_hull.py
+++ proj2/convex_hull.py
@@ -1,10 +1,10 @@
 from which_pyqt import PYQT_VER
 if PYQT_VER == 'PYQT5':
-	from PyQt5.QtCore import QLineF, QPointF, QObject
+  from PyQt5.QtCore import QLineF, QPointF, QObject
 elif PYQT_VER == 'PYQT4':
-	from PyQt4.QtCore import QLineF, QPointF, QObject
+  from PyQt4.QtCore import QLineF, QPointF, QObject
 else:
-	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
+  raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 
 
@@ -24,122 +24,158 @@ PAUSE = 0.25
 #
 class ConvexHullSolver(QObject):
 
-	# Class constructor
-	def __init__( self):
-		super().__init__()
-		self.pause = False
-		
+  # Class constructor
+  def __init__( self):
+    super().__init__()
+    self.pause = False
+    
 # Some helper methods that make calls to the GUI, allowing us to send updates
 # to be displayed.
 
-	def showTangent(self, line, color):
-		self.view.addLines(line,color)
-		if self.pause:
-			time.sleep(PAUSE)
+  def showTangent(self, line, color):
+    self.view.addLines(line,color)
+    if self.pause:
+      time.sleep(PAUSE)
 
-	def eraseTangent(self, line):
-		self.view.clearLines(line)
+  def eraseTangent(self, line):
+    self.view.clearLines(line)
 
-	def blinkTangent(self,line,color):
-		self.showTangent(line,color)
-		self.eraseTangent(line)
+  def blinkTangent(self,line,color):
+    self.showTangent(line,color)
+    self.eraseTangent(line)
 
-	def showHull(self, polygon, color):
-		self.view.addLines(polygon,color)
-		if self.pause:
-			time.sleep(PAUSE)
-		
-	def eraseHull(self,polygon):
-		self.view.clearLines(polygon)
-		
-	def showText(self,text):
-		self.view.displayStatusText(text)
-	
+  def showHull(self, polygon, color):
+    self.view.addLines(polygon,color)
+    if self.pause:
+      time.sleep(PAUSE)
+    
+  def eraseHull(self,polygon):
+    self.view.clearLines(polygon)
+    
+  def showText(self,text):
+    self.view.displayStatusText(text)
+  
 
 # This is the method that gets called by the GUI and actually executes
 # the finding of the hull
-	def compute_hull(self, points, pause, view):
-		self.pause = pause
-		self.view = view
-		assert(type(points) == list and type(points[0]) == QPointF)
+  def compute_hull(self, points, pause, view):
+    self.pause = pause
+    self.view = view
+    assert(type(points) == list and type(points[0]) == QPointF)
 
-		t1 = time.time()
-		# Sorts the points by increasing x-value
-		points.sort(key= lambda k: k.x())	# Overwrites points with same but new order
-		t2 = time.time()
+    t1 = time.time()
+    # Sorts the points by increasing x-value
+    points.sort(key= lambda pt: pt.x())	# Overwrites points with same but new order
+    t2 = time.time()
 
-		t3 = time.time()
-		polygon = self.solve(points)
-		t4 = time.time()
+    t3 = time.time()
+    polygon = self.solve(points)
+    t4 = time.time()
 
-		# when passing lines to the display, pass a list of QLineF objects.  Each QLineF
-		# object can be created with two QPointF objects corresponding to the endpoints
-		self.showHull(polygon,RED)
-		timeElapsed = (t2 - t1) + (t4 -t3)		# Time will be for sorting and solving combined
-		self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(timeElapsed))	# FIXME - Ask TA - Should time elapsed include the time it takes to sort
+    self.showTangent(polygon, BLUE);
 
-		polygon = []
+    # when passing ines to the display, pass a list of QLineF objects.  Each QLineF
+    # object can be created with two QPointF objects corresponding to the endpoints
+    self.showHull(polygon,RED)
+    timeElapsed = (t2 - t1) + (t4 -t3)		# Time will be for sorting and solving combined
+    self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(timeElapsed))	# FIXME - Ask TA - Should time elapsed include the time it takes to sort
 
-	'''Divide-and-conquer Convex Hull Solver'''
-	def solve(self, points):
-		# this is a dummy polygon of the first 3 unsorted points
-		polygon = [QLineF(points[i],points[(i+1)%3]) for i in range(3)]
-		return polygon
+    polygon = []
 
+  '''Divide-and-conquer Convex Hull Solver'''
+  def solve(self, points):
+    # Base case - combine # FIXME - FIGURE OUT HOW TO DO IT WITH 1 BASE CASE
+    if len(points) == 2:
+    	return QLineF(points[0], points[1])	# Might have to convert to list?
+    # Break up points into they are size 1
+    midIdx = len(points) // 2
+    leftHull = self.solve(points[:midIdx])	# First half
+    rightHull = self.solve(points[midIdx:])	# Second half
+    return self.combine(leftHull, rightHull)	
 
-	def findUpperTangent(self, leftHull, rightHull):
-		# rightmostLeft, leftmostRight = #max(leftHull), min(rightHull)
-		
-		pass#leftHull[-1], rightHull[0]	# Find leftmost and rightmost points
+  '''Finds upper tangent of each hull, then returns back indices for the 2 upper tangents'''
+  def findUpperTangent(self, leftHull, rightHull):
+    return leftHull[0], rightHull[0]
 
-		# temp = line(p,q)
-		# done = 0
-		# while not done:
-		# 	done = 1
-		# 	while temp != is not upper tangent to L:
-		# 		r = p's counterclockwise neighbor
-		# 		temp = line(r,q)
-		# 		p = r
-		# 		done = 0
-		# 	while temp is not upper tangent to R:
-		# 		r = q's clockwise neighbor
-		# 		temp = line(p,r)
-		# 		q = r
-		# 		done = 0
-		# return temp
+    # rightmostLeft, leftmostRight = #max(leftHull), min(rightHull)
+    
 
-		# def findLowerTangent(self, leftHull, rightHull):
-		# leftmost, rightmost = leftHull[0], rightHull[-1]	# Find leftmost and rightmost points
+    #leftHull[-1], rightHull[0]	# Find leftmost and rightmost points
 
-		# temp = line(p,q)
-		# done = 0
-		# while not done:
-		# 	done = 1
-		# 	while temp != is not lower tangent to L:
-		# 		r = p's clockwise neighbor
-		# 		temp = line(r,q)
-		# 		p = r
-		# 		done = 0
-		# 	while temp is not lower tangent to R:
-		# 		r = q's counter-clockwise neighbor
-		# 		temp = line(p,r)
-		# 		q = r
-		# 		done = 0
-		# return temp
+    # temp = line(p,q)
+    # done = 0
+    # while not done:
+    # 	done = 1
+    # 	while temp != is not upper tangent to L:
+    # 		r = p's counterclockwise neighbor
+    # 		temp = line(r,q)
+    # 		p = r
+    # 		done = 0
+    # 	while temp is not upper tangent to R:
+    # 		r = q's clockwise neighbor
+    # 		temp = line(p,r)
+    # 		q = r
+    # 		done = 0
+    # return temp
 
+  def findLowerTangent(self, leftHull, rightHull):
+    pass
+    # leftmost, rightmost = leftHull[0], rightHull[-1]	# Find leftmost and rightmost points
 
-	'''Sorts points in hull in clockwise order (array can be traversed backwards for counter-clockwise).'''
-	def sortHull(self):
-		pass
+    # temp = line(p,q)
+    # done = 0
+    # while not done:
+    # 	done = 1
+    # 	while temp != is not lower tangent to L:
+    # 		r = p's clockwise neighbor
+    # 		temp = line(r,q)
+    # 		p = r
+    # 		done = 0
+    # 	while temp is not lower tangent to R:
+    # 		r = q's counter-clockwise neighbor
+    # 		temp = line(p,r)
+    # 		q = r
+    # 		done = 0
+    # return temp
 
-	'''Returns index of item in hull (handles logic so that hull can loop circularly).'''
-	def at(self, hull, index):
-		return hull[index % len(hull)]
+  '''Combines 2 hulls together (returns a list of lines)'''
+  def combine(self, leftHull, rightHull):
+    # Find upper tangent connecting the hulls
+    leftUpperIdx, rightUpperIdx = self.findUpperTangent(leftHull, rightHull)
+    # Find lower tangent connecting the hulls
+    leftLowerIdx, rightLowerIdx = self.findLowerTangent(leftHull, rightHull)
+    # Connect the hulls with the 2 tangent lines
+    comboHull = Hull()
+    comboHull.append(leftHull[leftLowerIdx:leftUpperIdx])   # Part of left hull to keep
+    comboHull.append(QLineF(leftHull[leftUpperIdx], rightHull[rightUpperIdx]))    # Upper tangent connection
+    comboHull.append(rightHull[rightUpperIdx:rightLowerIdx])    # Part of right hull to keep
+    comboHull.append(QLineF(rightHull[rightLowerIdx], leftHull[leftLowerIdx]))    # Lower tangent connection
 
-	'''Finds the next clockwise neighbor in a hull.'''
-	def findCW(self, hull, currIndex):
-		return hull[currIndex + 1]
+    # return QLineF(leftHull[0],rightHull[0])
+    return comboHull
 
-	'''Finds the next counter-clockwise neighbor in a hull.'''
-	def findCCW(self, hull, currIndex):
-		return hull[currIndex - 1]
+  '''Calculates the slope of the line connecting 2 points (formula: slope = (y2 - y1) / (x2 - x1))'''
+  def findSlope(self, point1, point2):
+    return (point2.y() - point1.y()) / (point2.x() + point1.x())
+
+class Hull:   
+  hull = []   # List of QLineFs
+
+  def __init__(self, line):
+    self.hull = [line]
+
+  '''Overloading the [] operator - returns index of item in hull (handles logic so that hull can loop circularly).'''
+  def __getitem__(self, index):
+      return self.hull[index % len(self.hull)]
+
+  '''Finds the next clockwise neighbor in a hull.'''
+  def findCW(self, hull, currIndex):
+    return hull[currIndex + 1]
+
+  '''Finds the next counter-clockwise neighbor in a hull.'''
+  def findCCW(self, hull, currIndex):
+    return hull[currIndex - 1]
+
+  # '''Returns index of item in hull (handles logic so that hull can loop circularly).'''
+  # def at(self, hull, index):
+  #   return hull[index % len(hull)]
