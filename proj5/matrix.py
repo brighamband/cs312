56ad2f2504b71eef329b4b3f344c3592c2205f6f
Brigham Andersen <brighamband@gmail.com>
2021-11-13 18:56:33 -0700

    finished making reduced cost matrix function

---
+++ proj5/matrix.py
@@ -0,0 +1,34 @@
+import numpy as np
+import math
+
+matrix = np.array(
+    [
+        [math.inf, 5, 4, 3],
+        [3, math.inf, 8, 2],
+        [5, 3, math.inf, 9],
+        [6, 4, 3, math.inf],
+    ]
+)
+
+
+# Returns a reduced cost matrix (0s in every row and col with the adjusted differences) for a given cost matrix
+def reduce_cost_matrix(matrix):
+    # Find zero and difference for each row
+    row_mins = matrix.min(axis=1)
+
+    for i in range(len(row_mins)):
+        matrix[i, :] -= row_mins[i]
+
+    # Find zero and difference for each col
+    col_mins = matrix.min(axis=0)
+
+    for i in range(len(col_mins)):
+        if col_mins[i] > 0:
+            matrix[:, i] -= col_mins[i]
+
+    return matrix
+
+
+matrix = reduce_cost_matrix(matrix)
+
+print(matrix)
