ba6dcc5626ce1c8fbe84c785b0e55f6305e830e8
Brigham Andersen <brighamband@gmail.com>
2021-11-15 14:20:37 -0700

    started on greedy

--- proj5/TSPSolver.py
+++ proj5/TSPSolver.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 from which_pyqt import PYQT_VER
-if PYQT_VER == 'PYQT5':
-	from PyQt5.QtCore import QLineF, QPointF
-elif PYQT_VER == 'PYQT4':
-	from PyQt4.QtCore import QLineF, QPointF
+
+if PYQT_VER == "PYQT5":
+    from PyQt5.QtCore import QLineF, QPointF
+elif PYQT_VER == "PYQT4":
+    from PyQt4.QtCore import QLineF, QPointF
 else:
-	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
-
-
+    raise Exception("Unsupported Version of PyQt: {}".format(PYQT_VER))
 
 
 import time
@@ -18,16 +17,14 @@ import heapq
 import itertools
 
 
-
 class TSPSolver:
-	def __init__( self, gui_view ):
-		self._scenario = None
+    def __init__(self, gui_view):
+        self._scenario = None
 
-	def setupWithScenario( self, scenario ):
-		self._scenario = scenario
+    def setupWithScenario(self, scenario):
+        self._scenario = scenario
 
-
-	''' <summary>
+    """ <summary>
 		This is the entry point for the default solver
 		which just finds a valid random tour.  Note this could be used to find your
 		initial BSSF.
@@ -36,40 +33,39 @@ class TSPSolver:
 		time spent to find solution, number of permutations tried during search, the 
 		solution found, and three null values for fields not used for this 
 		algorithm</returns> 
-	'''
-	
-	def defaultRandomTour( self, time_allowance=60.0 ):
-		results = {}
-		cities = self._scenario.getCities()
-		ncities = len(cities)
-		foundTour = False
-		count = 0
-		bssf = None
-		start_time = time.time()
-		while not foundTour and time.time()-start_time < time_allowance:
-			# create a random permutation
-			perm = np.random.permutation( ncities )
-			route = []
-			# Now build the route using the random permutation
-			for i in range( ncities ):
-				route.append( cities[ perm[i] ] )
-			bssf = TSPSolution(route)
-			count += 1
-			if bssf.cost < np.inf:
-				# Found a valid route
-				foundTour = True
-		end_time = time.time()
-		results['cost'] = bssf.cost if foundTour else math.inf
-		results['time'] = end_time - start_time
-		results['count'] = count
-		results['soln'] = bssf
-		results['max'] = None
-		results['total'] = None
-		results['pruned'] = None
-		return results
+	"""
 
+    def defaultRandomTour(self, time_allowance=60.0):
+        results = {}
+        cities = self._scenario.getCities()
+        ncities = len(cities)
+        foundTour = False
+        count = 0
+        bssf = None
+        start_time = time.time()
+        while not foundTour and time.time() - start_time < time_allowance:
+            # create a random permutation
+            perm = np.random.permutation(ncities)
+            route = []
+            # Now build the route using the random permutation
+            for i in range(ncities):
+                route.append(cities[perm[i]])
+            bssf = TSPSolution(route)
+            count += 1
+            if bssf.cost < np.inf:
+                # Found a valid route
+                foundTour = True
+        end_time = time.time()
+        results["cost"] = bssf.cost if foundTour else math.inf
+        results["time"] = end_time - start_time
+        results["count"] = count
+        results["soln"] = bssf
+        results["max"] = None
+        results["total"] = None
+        results["pruned"] = None
+        return results
 
-	''' <summary>
+    """ <summary>
 		This is the entry point for the greedy solver, which you must implement for 
 		the group project (but it is probably a good idea to just do it for the branch-and
 		bound project as a way to get your feet wet).  Note this could be used to find your
@@ -79,39 +75,60 @@ class TSPSolver:
 		time spent to find best solution, total number of solutions found, the best
 		solution found, and three null values for fields not used for this 
 		algorithm</returns> 
-	'''
+	"""
 
-	def greedy( self,time_allowance=60.0 ):
-		pass
-	
-	
-	
-	''' <summary>
+    def greedy(self, time_allowance=60.0):
+        results = {}
+        cities = self._scenario.getCities()
+        num_cities = len(cities)
+        found_tour = False
+        count = 0
+        bssf = None
+        start_time = time.time()
+
+        while not found_tour and time.time() - start_time < time_allowance:
+            print("cs", cities)
+            # create a random permutation
+            # perm = np.random.permutation(num_cities)
+            # route = []
+            # # Now build the route using the random permutation
+            # for i in range(num_cities):
+            #     route.append(cities[perm[i]])
+            # bssf = TSPSolution(route)
+            # count += 1
+            # if bssf.cost < np.inf:
+            #     # Found a valid route
+            #     found_tour = True
+        # end_time = time.time()
+        # results["cost"] = bssf.cost if found_tour else math.inf
+        # results["time"] = end_time - start_time
+        # results["count"] = count
+        # results["soln"] = bssf
+        # results["max"] = None
+        # results["total"] = None
+        # results["pruned"] = None
+        # return results
+
+    """ <summary>
 		This is the entry point for the branch-and-bound algorithm that you will implement
 		</summary>
 		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
 		time spent to find best solution, total number solutions found during search (does
 		not include the initial BSSF), the best solution found, and three more ints: 
 		max queue size, total number of states created, and number of pruned states.</returns> 
-	'''
-		
-	def branchAndBound( self, time_allowance=60.0 ):
-		pass
+	"""
 
+    def branchAndBound(self, time_allowance=60.0):
+        pass
 
-
-	''' <summary>
+    """ <summary>
 		This is the entry point for the algorithm you'll write for your group project.
 		</summary>
 		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
 		time spent to find best solution, total number of solutions found during search, the 
 		best solution found.  You may use the other three field however you like.
 		algorithm</returns> 
-	'''
-		
-	def fancy( self,time_allowance=60.0 ):
-		pass
-		
+	"""
 
-
-
+    def fancy(self, time_allowance=60.0):
+        pass
