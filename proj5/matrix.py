11d0ea4e4a727355c8ff9786d42c68c493ebad0c
Brigham Andersen <brighamband@gmail.com>
2021-11-16 13:14:35 -0700

    added lower bound

--- proj5/matrix.py
+++ proj5/matrix.py
@@ -13,21 +13,25 @@ matrix = np.array(
 
 # Returns a reduced cost matrix (0s in every row and col with the adjusted differences) for a given cost matrix
 def reduce_cost_matrix(matrix):
+    lower_bound = 0
+
     # Find zero and difference for each row
     row_mins = matrix.min(axis=1)
 
     for i in range(len(row_mins)):
         matrix[i, :] -= row_mins[i]
+        lower_bound += row_mins[i]
 
     # Find zero and difference for each col
     col_mins = matrix.min(axis=0)
 
     for i in range(len(col_mins)):
         matrix[:, i] -= col_mins[i]
+        lower_bound += col_mins[i]
 
-    return matrix
+    return matrix, lower_bound
 
 
-matrix = reduce_cost_matrix(matrix)
+matrix, lower_bound = reduce_cost_matrix(matrix)
 
-print(matrix)
+print(matrix, lower_bound)
