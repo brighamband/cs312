322a97c4cecd1199facc16cb28b4725a6daf94b8
Brigham Andersen <brighamband@gmail.com>
2021-11-16 13:41:43 -0700

    Added the add path logic

--- proj5/matrix.py
+++ proj5/matrix.py
@@ -10,28 +10,56 @@ matrix = np.array(
     ]
 )
 
+lower_bound = 0
 
 # Returns a reduced cost matrix (0s in every row and col with the adjusted differences) for a given cost matrix
-def reduce_cost_matrix(matrix):
-    lower_bound = 0
-
+def reduce_cost_matrix(matrix, lower_bound):
     # Find zero and difference for each row
     row_mins = matrix.min(axis=1)
 
     for i in range(len(row_mins)):
-        matrix[i, :] -= row_mins[i]
-        lower_bound += row_mins[i]
+        if row_mins[i] != math.inf:  # Skip over eliminated rows
+            matrix[i, :] -= row_mins[i]
+            lower_bound += row_mins[i]
 
     # Find zero and difference for each col
     col_mins = matrix.min(axis=0)
 
     for i in range(len(col_mins)):
-        matrix[:, i] -= col_mins[i]
-        lower_bound += col_mins[i]
+        if col_mins[i] != math.inf:  # Skip over eliminated cols
+            matrix[:, i] -= col_mins[i]
+            lower_bound += col_mins[i]
 
     return matrix, lower_bound
 
 
-matrix, lower_bound = reduce_cost_matrix(matrix)
+matrix, lower_bound = reduce_cost_matrix(matrix, lower_bound)
+
+print(matrix, lower_bound)
+
+
+def add_path_and_update_matrix(matrix, row_idx, col_idx, parent_bound):
+    # Update parent bound to now have updated cost
+    parent_bound += matrix[row_idx, col_idx]
+
+    # Eliminate chosen row (set to inf)
+    matrix[row_idx, :] = math.inf
+
+    # Eliminate chosen col (set to inf)
+    matrix[:, col_idx] = math.inf
+
+    # Eliminate the opposite cell (don't let it go back)
+    last_path = False  # FIXME - Add in logic that checks to see if all nodes have been visited and it can go back
+    if not last_path:
+        matrix[col_idx][row_idx] = math.inf
+
+    # Reduce cost matrix again
+    matrix, parent_bound = reduce_cost_matrix(matrix, parent_bound)
+
+    return matrix, parent_bound
+
+
+# FIXME - NEED TO ACCOUNT FOR FACT THAT NAMES DO NOT CORRESPOND WITH INDICES
+matrix, lower_bound = add_path_and_update_matrix(matrix, 0, 2, lower_bound)
 
 print(matrix, lower_bound)
